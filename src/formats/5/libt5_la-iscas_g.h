fbea1bc6ddc999e6ea2f30eef504910212a7e55b
mkouril <mkouril>
2003-10-21 12:29:20 +0000

    Forgotten iscas header.

---
+++ src/formats/5/libt5_la-iscas_g.h
@@ -0,0 +1,68 @@
+/* A Bison parser, made by GNU Bison 1.875.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ID = 258,
+     P_AND = 259,
+     P_OR = 260,
+     P_NOT = 261,
+     P_INPUT = 262,
+     P_OUTPUT = 263
+   };
+#endif
+#define ID 258
+#define P_AND 259
+#define P_OR 260
+#define P_NOT 261
+#define P_INPUT 262
+#define P_OUTPUT 263
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+#line 39 "iscas_g.yy"
+typedef union YYSTYPE {
+    int         num;      /* For returning numbers.               */
+    char        id[200];  /* For returning ids.                   */
+    t_op2fn     op2fn;    /* For returning op2fn                  */
+    BDDNode     *bdd;     /* For returning exp                    */
+} YYSTYPE;
+/* Line 1248 of yacc.c.  */
+#line 59 "libt5_la-iscas_g.h"
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE iscas_lval;
+
+
+
