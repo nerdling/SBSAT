733302cc2613acee4d8b2c99cdc16e4ecd707701
mkouril <mkouril>
2004-01-13 17:29:29 +0000

    Added generated file

---
+++ confdefs.h
@@ -0,0 +1,80 @@
+
+#define PACKAGE_NAME "sbsat"
+#define PACKAGE_TARNAME "sbsat"
+#define PACKAGE_VERSION "2.1b-3"
+#define PACKAGE_STRING "sbsat 2.1b-3"
+#define PACKAGE_BUGREPORT "mkouril@ececs.uc.edu"
+#define PACKAGE "sbsat"
+#define VERSION "2.1b-3"
+#ifdef __cplusplus
+#include <stdlib.h>
+#endif
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define STDC_HEADERS 1
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDDEF_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_TERMIOS_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_STRING 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_ASSERT_H 1
+#define HAVE_IOSTREAM_H 1
+#define HAVE_FSTREAM_H 1
+#define HAVE_IOSTREAM 1
+#define HAVE_FSTREAM 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_TIMES_H 1
+#define HAVE_SYS_RESOURCE_H 1
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_CTYPE_H 1
+#define HAVE_MATH_H 1
+#define HAVE_REGEX_H 1
+#define HAVE_TERMCAP_H 1
+#define TIME_WITH_SYS_TIME 1
+#define HAVE_PTRDIFF_T 1
+#define HAVE_STDLIB_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_MMAP 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_SYS_SOCKET_H 1
+#define SELECT_TYPE_ARG1 int
+#define SELECT_TYPE_ARG234 (fd_set *)
+#define SELECT_TYPE_ARG5 (struct timeval *)
+#define RETSIGTYPE void
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_GETTIMEOFDAY 1
+#define HAVE_MEMMOVE 1
+#define HAVE_MEMSET 1
+#define HAVE_MUNMAP 1
+#define HAVE_SQRT 1
+#define HAVE_STRCASECMP 1
+#define HAVE_STRCHR 1
+#define HAVE_STRNCASECMP 1
+#define HAVE_STRRCHR 1
+#define HAVE_STD_NAMESPACE
+#define STD_NAMESPACE std::
+#define HAVE_USING_NAMESPACE_STD
+#define HAVE_USING_NAMESPACE_STD 1
+#define HAVE_TERMCAP_LIB
