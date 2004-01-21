66d45d47cd54a40849d999c04aa397210e8bdcce
mkouril <mkouril>
2004-01-21 13:43:00 +0000

    version update 2.1r-1

--- confdefs.h
+++ confdefs.h
@@ -1,11 +1,11 @@
 
 #define PACKAGE_NAME "sbsat"
 #define PACKAGE_TARNAME "sbsat"
-#define PACKAGE_VERSION "2.1b-3"
-#define PACKAGE_STRING "sbsat 2.1b-3"
+#define PACKAGE_VERSION "2.1r-1"
+#define PACKAGE_STRING "sbsat 2.1r-1"
 #define PACKAGE_BUGREPORT "mkouril@ececs.uc.edu"
 #define PACKAGE "sbsat"
-#define VERSION "2.1b-3"
+#define VERSION "2.1r-1"
 #ifdef __cplusplus
 #include <stdlib.h>
 #endif
