6f74693a7cac13b21f0940755fc8eb8226a043d2
fett <fett>
2004-07-12 19:24:15 +0000

    this searches for and finds the function type of variables.
      ~Sean

---
+++ src/preproc/do_findft.cc
@@ -0,0 +1,76 @@
+/* =========FOR INTERNAL USE ONLY. NO DISTRIBUTION PLEASE ========== */
+
+/*********************************************************************
+ Copyright 1999-2004, University of Cincinnati.  All rights reserved.
+ By using this software the USER indicates that he or she has read,
+ understood and will comply with the following:
+
+ --- University of Cincinnati hereby grants USER nonexclusive permission
+ to use, copy and/or modify this software for internal, noncommercial,
+ research purposes only. Any distribution, including commercial sale
+ or license, of this software, copies of the software, its associated
+ documentation and/or modifications of either is strictly prohibited
+ without the prior consent of University of Cincinnati.  Title to copyright
+ to this software and its associated documentation shall at all times
+ remain with University of Cincinnati.  Appropriate copyright notice shall
+ be placed on all software copies, and a complete copy of this notice
+ shall be included in all copies of the associated documentation.
+ No right is  granted to use in advertising, publicity or otherwise
+ any trademark,  service mark, or the name of University of Cincinnati.
+
+
+ --- This software and any associated documentation is provided "as is"
+
+ UNIVERSITY OF CINCINNATI MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS
+ OR IMPLIED, INCLUDING THOSE OF MERCHANTABILITY OR FITNESS FOR A
+ PARTICULAR PURPOSE, OR THAT  USE OF THE SOFTWARE, MODIFICATIONS, OR
+ ASSOCIATED DOCUMENTATION WILL NOT INFRINGE ANY PATENTS, COPYRIGHTS,
+ TRADEMARKS OR OTHER INTELLECTUAL PROPERTY RIGHTS OF A THIRD PARTY.
+
+ University of Cincinnati shall not be liable under any circumstances for
+ any direct, indirect, special, incidental, or consequential damages
+ with respect to any claim by USER or any third party on account of
+ or arising from the use, or inability to use, this software or its
+ associated documentation, even if University of Cincinnati has been advised
+ of the possibility of those damages.
+*********************************************************************/
+
+#include "sbsat.h"
+#include "sbsat_preproc.h"
+
+int Do_Find_FunctionType() {
+	char p[100];
+	affected = 0;
+	d3_printf1("FINDING FUNCTION TYPES - ");
+	D_3(
+		 sprintf(p, "{0:0/%ld}", nmbrFunctions);
+		 str_length = strlen(p);
+		 d3_printf1(p);
+	);
+	for(int i = 0; i < nmbrFunctions; i++) {
+		D_3(
+			 if (i % ((numinp/100)+1) == 0) {
+				 for(int iter = 0; iter<str_length; iter++)
+					d3_printf1("\b");
+				 sprintf(p, "{%ld:%d/%ld}", affected, i, nmbrFunctions);
+				 str_length = strlen(p);
+				 d3_printf1(p);
+			 }
+		);
+		if(functionType[i] == UNSURE) {
+			//find function type.
+			//check various methods.
+			//isOR = PLAINOR
+			//isAND_EQU = AND or OR
+			//isXOR = PLAINXOR
+			//isMINMAX = MINMAX
+			affected += findandset_fnType(i);
+		}
+	}
+
+	d3_printf1("\n");
+   d2e_printf1("\r                                         ");
+	
+	int ret = PREP_NO_CHANGE;
+	return ret;
+}
