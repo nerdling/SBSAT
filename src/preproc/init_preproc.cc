2f93791d6dbe91ea911341b0329ae680449e7b3e
fett <fett>
2006-04-11 21:38:11 +0000

    *** empty log message ***

--- src/preproc/init_preproc.cc
+++ src/preproc/init_preproc.cc
@@ -395,7 +395,7 @@ Finish_Preprocessing()
 					(100 * Setting_Neg) / (Total_inferences+div_zero));
 	d3_printf2 ("Total Inferences  - %d (100%%)\n\n", Total_inferences);
 
-	fprintf(stderr, "%d ", num_safe_assigns);
+	//fprintf(stderr, "%d ", num_safe_assigns);
 	//Stats();
 	
 	//Need to release these pointers
