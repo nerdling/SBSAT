3aafe4c567155c97673674d97bf908868bea1edb
anne <anne>
2007-05-30 05:52:46 +0000

    added minisat files

---
+++ src/solv_smurf/hr_minisat/hr_minisat.cc
@@ -0,0 +1,131 @@
+/* =========FOR INTERNAL USE ONLY. NO DISTRIBUTION PLEASE ========== */
+
+/*********************************************************************
+ Copyright 1999-2007, University of Cincinnati.  All rights reserved.
+ By using this software the USER indicates that he or she has read,
+ understood and will comply with the following:
+
+ --- University of Cincinnati hereby grants USER nonexclusive permission
+ to use, copy and/or modify this software for internal, noncommercial,
+ research purposes only. Any distribution, including commercial sale
+ or license, of this software, copies of the software, its associated
+ documentation and/or modifications of either is strictly prohibited
+ without the prior consent of University of Cincinnati.  Title to copyright
+ to this software and its associated documentation shall at all times
+ remain with University of Cincinnati.  Appropriate copyright notice shall
+ be placed on all software copies, and a complete copy of this notice
+ shall be included in all copies of the associated documentation.
+ No right is  granted to use in advertising, publicity or otherwise
+ any trademark,  service mark, or the name of University of Cincinnati.
+
+
+ --- This software and any associated documentation is provided "as is"
+
+ UNIVERSITY OF CINCINNATI MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS
+ OR IMPLIED, INCLUDING THOSE OF MERCHANTABILITY OR FITNESS FOR A
+ PARTICULAR PURPOSE, OR THAT  USE OF THE SOFTWARE, MODIFICATIONS, OR
+ ASSOCIATED DOCUMENTATION WILL NOT INFRINGE ANY PATENTS, COPYRIGHTS,
+ TRADEMARKS OR OTHER INTELLECTUAL PROPERTY RIGHTS OF A THIRD PARTY.
+
+ University of Cincinnati shall not be liable under any circumstances for
+ any direct, indirect, special, incidental, or consequential damages
+ with respect to any claim by USER or any third party on account of
+ or arising from the use, or inability to use, this software or its
+ associated documentation, even if University of Cincinnati has been advised
+ of the possibility of those damages.
+*********************************************************************/
+
+#include "sbsat.h"
+#include "sbsat_solver.h"
+#include "solver.h"
+
+t_arrVarScores *arrMiniSatVarScores = NULL;
+
+ITE_INLINE int
+HrMiniSatInit()
+{
+   InitMiniSatHeurArrays(gnMaxVbleIndex);
+
+   procHeurUpdate = HrMiniSatUpdate;
+   procHeurFree = HrMiniSatFree;
+   procHeurSelect = MiniSat_OptimizedHeuristic;
+   procHeurAddLemma = AddMiniSatHeuristicInfluence;
+   procHeurAddLemmaSpace = AddMiniSatSpaceHeuristicInfluence;
+   procHeurRemoveLemma = RemoveMiniSatHeuristicInfluence;
+
+   arrMiniSatVarScores = (t_arrVarScores*)ite_calloc(gnMaxVbleIndex, 
+         sizeof(t_arrVarScores), 9, "arrMiniSatVarScores");
+
+   for(int i=0;i<nNumFuncs;i++)
+   {
+      for(int j=0;arrFnAndTypes[j];j++) {
+         if (arrSolverFunctions[i].nType == arrFnAndTypes[j]) {
+            AddMiniSatBlockHeuristicInfluence(arrSolverFunctions[i].fn_and.pLongLemma);
+
+            for(int k=0;k<arrSolverFunctions[i].fn_and.rhsVbles.nNumElts;k++)
+               AddMiniSatBlockHeuristicInfluence(arrSolverFunctions[i].fn_and.arrShortLemmas[k]);
+
+            break;
+         }
+      }
+   }
+/*
+   for(int i=1;i<gnMaxVbleIndex;i++)
+   {
+      arrLemmaVbleCountsPos[i] += arrAFS[i].nNumOneAFS;
+      arrLemmaVbleCountsNeg[i] += arrAFS[i].nNumOneAFS;
+   }
+*/
+   //return HrMiniSatUpdate();
+   return NO_ERROR;
+}
+
+ITE_INLINE int
+HrMiniSatFree()
+{
+   ite_free((void**)&arrMiniSatVarScores);
+   DeleteMiniSatHeurArrays();
+   return NO_ERROR;
+}
+
+ITE_INLINE int
+HrMiniSatUpdate()
+{
+  //divide counters -- decay 5%
+  for(int i = 1; i < gnMaxVbleIndex; i++){
+    //arrMiniSatVarScores[i].pos = arrMiniSatVarScores[i].pos / 4;
+    //arrMiniSatVarScores[i].neg = arrMiniSatVarScores[i].neg / 4;
+    arrLemmaVbleCountsPos[i] = arrLemmaVbleCountsPos[i] - (arrLemmaVbleCountsPos[i] / 20);
+    arrLemmaVbleCountsNeg[i] = arrLemmaVbleCountsNeg[i] - (arrLemmaVbleCountsNeg[i] / 20);
+  }
+
+
+  for (int i = 1; i<gnMaxVbleIndex; i++){
+  
+    d9_printf4("%d: (pos count = %d, neg count = %d)\n", i, arrLemmaVbleCountsPos[i], arrLemmaVbleCountsNeg[i]);
+    
+    //if the variable is positive, increment .pos
+    arrMiniSatVarScores[i].pos = arrLemmaVbleCountsPos[i];
+    //arrMiniSatVarScores[i].last_count_pos = arrLemmaVbleCountsPos[i];
+    arrMiniSatVarScores[i].neg = arrLemmaVbleCountsNeg[i];
+    //arrMiniSatVarScores[i].last_count_neg = arrLemmaVbleCountsPos[i];
+  }
+  
+  return NO_ERROR;
+}
+
+
+
+#define HEUR_WEIGHT(x,i) (arrLemmaVbleCountsPos[i] > arrLemmaVbleCountsNeg[i] ?  arrLemmaVbleCountsPos[i]\
+ : arrLemmaVbleCountsNeg[i]);
+
+#define HEUR_FUNCTION MiniSat_AllVarChoiceHeuristic
+#define HEUR_SIGN(nBestVble, multPos, multNeg) \
+  (arrLemmaVbleCountsPos[nBestVble] > arrLemmaVbleCountsNeg[nBestVble] ? \
+   BOOL_TRUE:BOOL_FALSE)
+#include "hr_choice.cc"
+
+
+#undef HEUR_WEIGHT
+#undef HEUR_SIGN
+#undef HEUR_FUNCTION
---
+++ src/solv_smurf/hr_minisat/hr_minisat_update.cc
@@ -0,0 +1,184 @@
+/* =========FOR INTERNAL USE ONLY. NO DISTRIBUTION PLEASE ========== */
+
+/*********************************************************************
+ Copyright 1999-2007, University of Cincinnati.  All rights reserved.
+ By using this software the USER indicates that he or she has read,
+ understood and will comply with the following:
+
+ --- University of Cincinnati hereby grants USER nonexclusive permission
+ to use, copy and/or modify this software for internal, noncommercial,
+ research purposes only. Any distribution, including commercial sale
+ or license, of this software, copies of the software, its associated
+ documentation and/or modifications of either is strictly prohibited
+ without the prior consent of University of Cincinnati.  Title to copyright
+ to this software and its associated documentation shall at all times
+ remain with University of Cincinnati.  Appropriate copyright notice shall
+ be placed on all software copies, and a complete copy of this notice
+ shall be included in all copies of the associated documentation.
+ No right is  granted to use in advertising, publicity or otherwise
+ any trademark,  service mark, or the name of University of Cincinnati.
+
+
+ --- This software and any associated documentation is provided "as is"
+
+ UNIVERSITY OF CINCINNATI MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS
+ OR IMPLIED, INCLUDING THOSE OF MERCHANTABILITY OR FITNESS FOR A
+ PARTICULAR PURPOSE, OR THAT  USE OF THE SOFTWARE, MODIFICATIONS, OR
+ ASSOCIATED DOCUMENTATION WILL NOT INFRINGE ANY PATENTS, COPYRIGHTS,
+ TRADEMARKS OR OTHER INTELLECTUAL PROPERTY RIGHTS OF A THIRD PARTY.
+
+ University of Cincinnati shall not be liable under any circumstances for
+ any direct, indirect, special, incidental, or consequential damages
+ with respect to any claim by USER or any third party on account of
+ or arising from the use, or inability to use, this software or its
+ associated documentation, even if University of Cincinnati has been advised
+ of the possibility of those damages.
+*********************************************************************/
+
+#include "sbsat.h"
+#include "sbsat_solver.h"
+#include "solver.h"
+
+double *arrMiniSatHeurScoresPos;
+double *arrMiniSatHeurScoresNeg;
+extern int *arrLemmaVbleCountsPos;
+extern int *arrLemmaVbleCountsNeg;
+extern int *arrLastLemmaVbleCountsPos;
+extern int *arrLastLemmaVbleCountsNeg;
+
+ITE_INLINE
+void
+InitMiniSatHeurArrays (int nMaxVbleIndex)
+{
+   arrMiniSatHeurScoresPos = (double*)ite_calloc(nMaxVbleIndex+1, sizeof(double), 9, "arrMiniSatHeurScoresPos");
+   arrMiniSatHeurScoresNeg = (double*)ite_calloc(nMaxVbleIndex+1, sizeof(double), 9, "arrMiniSatHeurScoresNeg");
+   arrLemmaVbleCountsPos = (int*)ite_calloc(nMaxVbleIndex+1, sizeof(int), 9, "arrLemmaVbleCountsPos");
+   arrLemmaVbleCountsNeg = (int*)ite_calloc(nMaxVbleIndex+1, sizeof(int), 9, "arrLemmaVbleCountsNeg");
+   arrLastLemmaVbleCountsPos = (int*)ite_calloc(nMaxVbleIndex+1, sizeof(int), 9, "arrLastLemmaVbleCountsPos");
+   arrLastLemmaVbleCountsNeg = (int*)ite_calloc(nMaxVbleIndex+1, sizeof(int), 9, "arrLastLemmaVbleCountsNeg");
+
+   for (int i = 0; i <= nMaxVbleIndex; i++)
+   {
+      arrMiniSatHeurScoresPos[i] = 0.0;
+      arrMiniSatHeurScoresNeg[i] = 0.0;
+      arrLemmaVbleCountsPos[i] = 1; // to satisfy the heuristic
+      arrLemmaVbleCountsNeg[i] = 1; // to satisfy the heuristic
+      arrLastLemmaVbleCountsPos[i] = 0;
+      arrLastLemmaVbleCountsNeg[i] = 0;
+   }
+
+}
+
+ITE_INLINE
+void
+DeleteMiniSatHeurArrays ()
+{
+   ite_free((void**)&arrMiniSatHeurScoresPos);
+   ite_free((void**)&arrMiniSatHeurScoresNeg);
+   ite_free((void**)&arrLemmaVbleCountsPos);
+   ite_free((void**)&arrLemmaVbleCountsNeg);
+   ite_free((void**)&arrLastLemmaVbleCountsPos);
+   ite_free((void**)&arrLastLemmaVbleCountsNeg);
+}
+
+//#define fWght 1000;
+//
+ITE_INLINE void
+AddMiniSatSpaceHeuristicInfluence(int *arr, int num)
+{
+ //  fprintf(stderr, "num=%d\n", num);
+   for(int i=0; i<num; i++)
+   {
+      int nLiteral = arr[i];
+//      fprintf(stderr, "lit=%d, ", nLiteral);
+      if (nLiteral >= 0)
+      {
+         //arrMiniSatHeurScoresPos[nLiteral] += fWght;
+         arrLemmaVbleCountsPos[nLiteral]++;
+      }
+      else
+      {
+         //arrMiniSatHeurScoresNeg[-nLiteral] += fWght;
+         arrLemmaVbleCountsNeg[-nLiteral]++;
+      }
+   }
+}
+
+ITE_INLINE void
+AddMiniSatBlockHeuristicInfluence(LemmaBlock *pLemmaBlock)
+{
+   int *arrLits = pLemmaBlock->arrLits;
+   int nLemmaLength = arrLits[0];
+   int nLiteral;
+   int nLitIndex, nLitIndexInBlock;
+
+   //double fWght = arrAndEqFalseWght[nLemmaLength].fPos * 10000;
+   //double fWght = 1000;
+
+   for (nLitIndex = 1, nLitIndexInBlock = 1;
+         nLitIndex <= nLemmaLength; nLitIndex++, nLitIndexInBlock++)
+   {
+      if (nLitIndexInBlock == LITS_PER_LEMMA_BLOCK)
+      {
+         nLitIndexInBlock = 0;
+         pLemmaBlock = pLemmaBlock->pNext;
+         arrLits = pLemmaBlock->arrLits;
+      }
+      nLiteral = arrLits[nLitIndexInBlock];
+      if (nLiteral >= 0)
+      {
+         //arrMiniSatHeurScoresPos[nLiteral] += fWght;
+         arrLemmaVbleCountsPos[nLiteral]++;
+      }
+      else
+      {
+         //arrMiniSatHeurScoresNeg[-nLiteral] += fWght;
+         arrLemmaVbleCountsNeg[-nLiteral]++;
+      }
+   }
+}
+
+ITE_INLINE void
+AddMiniSatHeuristicInfluence(LemmaInfoStruct * pLemmaInfo)
+{
+   LemmaBlock *pLemmaBlock = pLemmaInfo->pLemma;
+   AddMiniSatBlockHeuristicInfluence(pLemmaBlock);
+}
+
+
+ITE_INLINE void
+RemoveMiniSatHeuristicInfluence (LemmaInfoStruct * pLemmaInfo)
+   // Updates : arrMiniSatHeurScoresPos[], arrMiniSatHeurScoresNeg[].
+   // Remove the lemma's influence on the search heuristic.
+{
+   LemmaBlock *pLemmaBlock = pLemmaInfo->pLemma;
+   int *arrLits = pLemmaBlock->arrLits;
+   int nLemmaLength = arrLits[0];
+   int nLiteral;
+   int nLitIndex, nLitIndexInBlock;
+
+   //double fWght = arrAndEqFalseWght[nLemmaLength].fPos * 10000;
+   //double fWght = 1000;
+
+   for (nLitIndex = 1, nLitIndexInBlock = 1;
+         nLitIndex <= nLemmaLength; nLitIndex++, nLitIndexInBlock++)
+   {
+      if (nLitIndexInBlock == LITS_PER_LEMMA_BLOCK)
+      {
+         nLitIndexInBlock = 0;
+         pLemmaBlock = pLemmaBlock->pNext;
+         arrLits = pLemmaBlock->arrLits;
+      }
+      nLiteral = arrLits[nLitIndexInBlock];
+      if (nLiteral >= 0)
+      {
+         //arrMiniSatHeurScoresPos[nLiteral] -= fWght;
+         arrLemmaVbleCountsPos[nLiteral]--;
+      }
+      else
+      {
+         //arrMiniSatHeurScoresNeg[-nLiteral] -= fWght;
+         arrLemmaVbleCountsNeg[-nLiteral]--;
+      }
+   }
+}
