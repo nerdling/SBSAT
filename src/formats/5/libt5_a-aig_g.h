43c1792f7eaa42386fb673b808d15d9772693dce
fett <fett>
2008-05-11 00:24:06 +0000

    *** empty log message ***

---
+++ src/formats/5/libt5_a-aig_g.h
@@ -0,0 +1,66 @@
+/* A Bison parser, made by GNU Bison 2.1.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     UNS_INT = 258,
+     P_AIG = 259,
+     COMMENT_HEADER = 260,
+     STRING = 261,
+     IO_IDENTIFIER = 262
+   };
+#endif
+/* Tokens.  */
+#define UNS_INT 258
+#define P_AIG 259
+#define COMMENT_HEADER 260
+#define STRING 261
+#define IO_IDENTIFIER 262
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+#line 19 "aig_g.yy"
+typedef union YYSTYPE {
+    int  num;      /* For returning numbers.               */
+    char id[200];  /* For returning ids.                   */
+    BDDNode *bdd;  /*                                      */
+} YYSTYPE;
+/* Line 1447 of yacc.c.  */
+#line 58 "libt5_a-aig_g.h"
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE aig_lval;
+
+
+
