b61409d9aa2a0b4e6923ee9873e0c9069c138e14
Jeremy Lavergne <jeremy@lavergne.gotdns.org>
2011-05-09 16:22:22 -0400

    convert d3_printfY() warnings to dX_printf(Y,)

--- src/preproc/do_strength.cc
+++ src/preproc/do_strength.cc
@@ -55,22 +55,16 @@ int Do_Strength() {
 	d3_printf1("STRENGTHENING - ");
 	affected = 0;
 	char p[100];
-   D_3(
-		 sprintf(p, "{0:0/%d}", nmbrFunctions);
-		 str_length = strlen(p);
-		 d3_printf1(p);
-	);
+    sprintf(p, "{0:0/%d}", nmbrFunctions);
+    str_length = dX_printf(3, p);
 	for (int x = 0; x < nmbrFunctions; x++)
 	  {
-		  D_3(
-				if (x % ((nmbrFunctions/100)+1) == 0) {
-					for(int iter = 0; iter<str_length; iter++)
-					  d3_printf1("\b");
-					sprintf(p, "{%ld:%d/%d}", affected, x, nmbrFunctions);
-					str_length = strlen(p);
-					d3_printf1(p);
-				}
-		  );
+        if (x % ((nmbrFunctions/100)+1) == 0) {
+            for(int iter = 0; iter<str_length; iter++)
+                dX_printf(3, "\b");
+            sprintf(p, "{%ld:%d/%d}", affected, x, nmbrFunctions);
+            str_length = dX_printf(3, p);
+        }
 
 		  //if (term_getchar()=='f') Do Fast Foward
 		  
