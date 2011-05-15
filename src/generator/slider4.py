6b85442367cb1278f933555df904c09796c3f9fb
Jeremy Lavergne <jeremy@lavergne.gotdns.org>
2011-05-15 09:16:21 -0400

    add unsat to slider4

--- src/generator/slider4.py
+++ src/generator/slider4.py
@@ -6,22 +6,24 @@ from optparse import OptionParser
 Please note that UNSAT version seems to be harder than SAT.
 
 Disclaimer: no formal analysis was done to verify SAT and UNSAT
-This means that for some n SAT might return UNSAT and UNSAT might return SAT problem.
+This means that for some n SAT might return UNSAT and UNSAT might return SAT.
 Please use the solver to verify UN/SAT.
 
-I have tested n for mulple of 10 starting 20.
+I have tested n for multiples of 10 starting 20.
 SAT instances seem to have at most 2 solutions.
 '''
 
-def slider4(size, sat = True):
+def slider4(size, sat = 1):
     assert(size >= 20)
     s = size/20
+    type = '' if sat else 'UN'
+    order = [5,4] if sat else [4,5]
     output = []
 
     output.append("p bdd %d %d" % (size, size))
-    output.append("; automatically generated SAT slider2 with n=%d " % size)
+    output.append("; automatically generated %sSAT slider2 with n=%d " % (type, size))
     output.append("; Disclaimer: no formal analysis was done to verify SAT and UNSAT")
-    output.append("#define add_state1(1, 2, 3, 5, 4, 6)")
+    output.append("#define add_state1(1, 2, 3, %d, %d, 6)" % (order[0], order[1]))
     output.append("#equ(xor(1, and(-3, 5), nand(6, 4)), ite(2, or(5, -6), -5)))")
 
     items = [1, 2*s+3, 2*s+1, size/2-1-3*s, size/2-1, size/2]
@@ -40,51 +42,6 @@ def slider4(size, sat = True):
         row = 3 if row == 2 else 2
 
     return '\n'.join(output)
-
-def slider4_unsat(out_type, size):
-    # 80: 1675229 124.090s 
-    # {1, 11,  9, 27, 39, 40 }
-    # {1,  7, 10, 21, 33, 35, 40}
-    # int start1[6] = {1, 17-6, 15-6, 24+3, 33+6, size/2};
-    # int start2[7] = {1, 12-5, 16-6, 18+3, 27+6, 29+6, size/2};
-
-    # {1, 11,  9, 27, 39, 40 }
-    # {1,  7, 10, 23, 31, 35, 40}
-    # int start1[6] = {1, 17-6, 15-6, 24+3, 33+6, size/2};
-    # int start2[7] = {1, 12-5, 16-6, 18+5, 27+4, 29+6, size/2};
-    #
-    # good one for 80 and more unsat
-    # int s=size/20;
-    # int start1[6] = {1, 2*s+3, 2*s+1, size/2-1-3*s, size/2-1, size/2};
-    # int start2[7] = {1, 2*s-1, 2*s+2, size/2-1-4*s, size/2-1-2*s, size/2-1-s, size/2};
-    #
-    # UNSAT
-
-    # fprintf(stderr, "{%d, %d, %d, %d, %d, %d}\n", start1[0], start1[1], start1[2], start1[3], start1[4], start1[5]);
-    # fprintf(stderr, "{%d, %d, %d, %d, %d, %d, %d}\n", start2[0], start2[1], start2[2], start2[3], start2[4], start2[5], start2[6]);
-    print "p bdd {0:d), {0:d)".format(size)
-    print("; automatically generated UNSAT slider4 with size={0:d}".format(size))
-    print("; Disclaimer: no formal analysis was done to verify SAT and UNSAT")
-
-    #first function
-    print("#define add_state1(1, 2, 3, 4, 5, 6)")
-    print("#equ(xor(1, and(-3, 5), nand(6, 4)), ite(2, or(5, -6), -5)))")
-    
-    for i in range(0, size/2):
-        print "add_state1{0}".format(start1)
-        for item in start1:
-            start1[item] += 1
-    
-    #Second and Third Function
-    print("#define add_state2(1, 2, 3, 4, 5, 6, 7)")
-    print("#equ(6, xor(-1, xor(3, and(-4, 5), 4), equ(7, 2)))")
-    print("#define add_state3(1, 2, 3, 4, 5, 6, 7)")
-    print("#not(equ(6, xor(-1, xor(3, and(-4, 5), 4), equ(7, 2))))")
-    for i in range(0, size-size/2):
-        print "add_state{0}".format([i%2+2] + start2)
-        for item in range(len(start2)):
-            start2[item] += 1
-
 
 #main method. Option parsing
 def main():
